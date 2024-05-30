8062be5a4f61e6cb9a3f261d829127d8c1bbb22d
javier <ojalfarob@gmail.com>
2024-05-30 08:42:55 -0600

    fixing problems with entry Post - put

--- src/main.py
+++ src/main.py
@@ -25,3 +25,34 @@ def read_user(user_id:int,db:_orm.Session=_fastapi.Depends(_services.get_db)):
     if db_user is None:
         raise _fastapi.HTTPException(status_code=404,detail="sorry this user does not exist")
     return db_user
+
+
+@app.post("/users/{user_id}/posts/",response_model=_schemas.Post)
+def create_post(user_id:int,post:_schemas.PostCreate,db:_orm.Session = _fastapi.Depends(_services.get_db),):
+    db_user = _services.get_user(db=db,user_id=user_id)
+    if db_user is None:
+        raise _fastapi.HTTPException(status_code=404,detail="sorry this user does not exist")
+    return _services.create_post(db=db,post=post,user_id=user_id)
+
+
+@app.get("/posts/",response_model=List[_schemas.Post])
+def read_posts(skip:int=0,limit: int=10,db:_orm.Session= _fastapi.Depends(_services.get_db),):
+    posts = _services.get_posts(db=db,skip=skip,limit=limit)
+    return posts
+
+@app.get("/post/{post_id}",response_model=_schemas.Post)
+def read_post(post_id:int,db:_orm.Session=_fastapi.Depends(_services.get_db)):
+    db_post= _services.get_post(db=db,post_id=post_id)
+    if db_post is None:
+        raise _fastapi.HTTPException(status_code=404,detail="sorry this post does not exist")
+    return db_post
+
+
+@app.delete("/post/{post_id}")
+def delete_post(post_id:int,db:_orm.Session=_fastapi.Depends(_services.get_db)):
+    _services.delete_post(db=db,post_id=post_id)
+    return {"message":f"succesfully deleted post whit id: {post_id}"}
+
+@app.put("/post/{post_id}",response_model=_schemas.Post)
+def update_post(post_id:int,post:_schemas.PostCreate,db:_orm.Session=_fastapi.Depends(_services.get_db)):
+    return _services.update_post(db=db,post=post,post_id=post_id)
--- src/services.py
+++ src/services.py
@@ -1,5 +1,6 @@
 import database as _database,models as _models, schemas as _schemas
 import sqlalchemy.orm as _orm
+import datetime as _dt
 
 def create_database():
     return _database.Base.metadata.create_all(bind=_database.engine)
@@ -31,3 +32,31 @@ def get_users(db: _orm.Session,skip:int,limit:int):
 
 def get_user(db: _orm.Session,user_id:int):
     return db.query(_models.User).filter(_models.User.id ==user_id).first()
+
+
+def create_post(db:_orm.Session,post:_schemas.PostCreate,user_id:int):
+    post = _models.Post(**post.dict(),owner_id=user_id)
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
+
+def get_posts(db: _orm.Session,skip:int,limit:int):
+    return db.query(_models.Post).offset(skip).limit(limit).all()
+
+def get_post(db: _orm.Session,post_id:int):
+    return db.query(_models.Post).filter(_models.Post.id ==post_id).first()
+
+def delete_post(db: _orm.Session,post_id:int):
+    db.query(_models.Post).filter(_models.Post.id ==post_id).delete()
+    db.commit()
+
+
+def update_post(db: _orm.Session,post:_schemas.PostCreate,post_id:int):
+    db_post = get_post(db=db,post_id=post_id)
+    db_post.title= post.title
+    db_post.content= post.content
+    db_post.date_last_updated = _dt.datetime.now()
+    db.commit()
+    db.refresh(db_post)
+    return db_post
